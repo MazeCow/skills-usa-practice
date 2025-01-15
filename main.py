9ef1d439a560940756f1f010430ce311e81f6727
CMP - Isaac Trost <CMP_IsTrost@ects.org>
2025-01-15 14:39:42 -0500

    test

--- main.py
+++ main.py
@@ -3,7 +3,7 @@
 
 from dataclasses import dataclass
 from tabulate import tabulate
-import json
+import os
 
 @dataclass
 class Team:
@@ -28,69 +28,65 @@ class Team:
     def division_win_percent(self):
         return self.division_wins / (self.division_wins + self.division_losses)
     
+    @property
+    def table_values(self):
+        return [self.team_name, f"{self.wins}-{self.losses}", f"{self.win_percent:.3f}", f"{self.conference_wins}-{self.conference_losses}", f"{self.division_wins}-{self.division_losses}"]
+    
+    
 def parse_csv() -> dict:
+    # Data file location.
     DATA_PATH = "./data.csv"
+    
     # Object to store the extracted team data.
     districts: dict = {}
     with open(DATA_PATH, "r") as f:
         # Skips csv headers.
         f.readline()
+        
         # Enumerate through each line the file.
         for line in f:
             # Remove new line characters.
             line = line.replace("\n", "")
+            
             # Split the line into values.
             values = line.split(",")
 
-            # Unpack the variables and calculate new ones.
+            # Unpack the variables.
             division = str(values[1])
             team_name = str(values[0])
             wins = int(values[2])
             losses = int(values[3])
-            win_percent = wins / (wins + losses)
             conference_wins = int(values[4])
             conference_losses = int(values[5])
-            conference_win_percent = conference_wins / (conference_wins / conference_losses)
             division_wins = int(values[6])
             division_losses = int(values[7])
-            division_win_percent = division_wins / (division_wins + division_losses)
             
             # Create an object with the values from the csv.
-            new_team = {
-                "team_name": team_name,
-                "wins": wins,
-                "losses": losses,
-                "win_percent": win_percent,
-                "conference_wins": conference_wins,
-                "conference_losses": conference_losses,
-                "conference_win_percent": conference_win_percent,
-                "division_wins": division_wins,
-                "division_losses": division_losses,
-                "division_win_percent": division_win_percent
-            }
+            new_team = Team(team_name, division, wins, losses, conference_wins, conference_losses, division_wins, division_losses)
 
             # Create a new division if it was previously not found.
             if not division in districts:
                 districts[division] = []
+                
             # Add each team to their respective division.
             districts[division].append(new_team)
     # Return the divisions.
     return districts
             
-def compare_teams(team_1: Team, team_2: Team):
-    """Returns 1 if team one has better stats. Otherwise returns 0.
+            
+def compare_teams(team_1: Team, team_2: Team) -> int:
+    """Returns 1 if team one has better stats, otherwise returns 0.
 
     Args:
-        team_1 (Team), team_2 (Team)
+        team_1 (Team): The first team to compare.
+        team_2 (Team): The second team to compare.
     """
-    if team_1["win_percent"] > team_2["win_percent"]:
-        return 1
-    elif team_1["win_percent"] == team_2["win_percent"]:
-        if team_1["conference_win_percent"] > team_2["conference_win_percent"]:
+    stats = ["win_percent", "conference_win_percent", "division_win_percent"]
+    for stat in stats:
+        if getattr(team_1, stat) > getattr(team_2, stat):
             return 1
-        elif team_1["conference_win_percent"] == team_2["conference_win_percent"]:
-            if team_1["division_win_percent"] > team_2["division_win_percent"]:
-                return 1
+        elif getattr(team_1, stat) < getattr(team_2, stat):
+            return 0
     return 0
             
             
@@ -117,66 +113,89 @@ def sort_team(team_list: list[Team]) -> list[Team]:
     return team_list
         
         
-def sort_divisions(divisions_obj: dict[list[Team]]):
+def sort_divisions(division_data: dict[list[Team]]):
     # Enumerate through the divisions dict.
-    for key in divisions_obj:
-        division_teams = divisions_obj[key]
-        divisions_obj[key] = sort_team(division_teams)
+    for key in division_data:
+        division_teams = division_data[key]
+        division_data[key] = sort_team(division_teams)
 
-    return divisions_obj
-        
-# Converts data objects to json.
-def to_json(obj):
-    return json.dumps(obj, default=lambda obj: obj.__dict__, indent=2)
-    
-    
-def filter_table_properties(data, allowed_keys):
-    """
-    Removes unneeded properties from a dictionary, keeping only the allowed keys.
-    
+    return division_data
+
+
+def prepare_table_data(division_data: dict[list[Team]]) -> dict[list[Team]]:
+    """Prepares the divisions dictionary for table use.
+
     Args:
-        data (dict): The original dictionary.
-        allowed_keys (list or set): Keys to retain in the dictionary.
-    
+        divisions (dict[list[Team]]): A dictionary with a key-value pair of a district and a list of teams.
+
     Returns:
-        dict: A filtered dictionary with only the allowed keys.
+        dict[list[Team]]: A dictionary with 
     """
-    return {key: data[key] for key in allowed_keys if key in data}
 
-
-def prepare_table_data(divisions):
     table_data = {}
-    for key in divisions:
+    for key in division_data:
         table_data[key] = []
-        for team in divisions[key]:
-            table_data[key].append([
-                team["team_name"],
-                f"{team["wins"]}-{team["losses"]}",
-                f"{team["win_percent"]:.3f}",
-                f"{team["conference_wins"]}-{team["conference_losses"]}",
-                f"{team["division_wins"]}-{team["division_losses"]}"
-            ])
+        for team in division_data[key]:
+            table_data[key].append(team.table_values)
     return table_data
 
 
-
-def print_divisions(table_data: dict):
+def print_division_tables(table_data: dict):
+    # Headers
     headers = ["Team Name", "W-L", "Win %", "Conf W-L", "Div W-L"]
     
+    # Prints each division name and it's teams.
     for key in table_data:
         print(key)
         print(tabulate(table_data[key], headers=headers, tablefmt="rounded_grid"))
         print()
-        
 
+
+def get_matchups(division_data: dict[list[Team]]):
+    best_teams = []
+    worst_teams = []
+   
+    while len(division_data) > 1:
+        worst_team = None
+        best_team = None
+        for key in division_data:
+            for team in division_data[key]:
+                if team in best_teams + worst_teams:
+                    continue
+                
+                if worst_team is None:
+                    worst_team = team
+                elif team.win_percent < worst_team.win_percent:
+                    worst_team = team
+                        
+                if best_team is None:
+                    best_team = team
+                elif team.win_percent > best_team.win_percent:
+                    best_team = team
+        best_teams.append(best_team)
+        worst_teams.append(worst_team)
+    return [best_teams, worst_teams]
+        
+        
+        
+            
 
 def main():
-    teams_data = parse_csv()
-    sorted_divisions = sort_divisions(teams_data)
-    table_data = prepare_table_data(sorted_divisions)
-    print_divisions(table_data)
+    os.system('cls')
     
-
+    # Parse the csv and sort by team win rate in each division.
+    teams_data = sort_divisions(parse_csv())
+    
+    # Parse divisions data so it is usable in a table.
+    table_data = prepare_table_data(teams_data)
+    
+    # Print each division's table.
+    print_division_tables(table_data)
+    
+    print(get_matchups(teams_data))
+    
+    
+    
 
 if __name__ == "__main__":
     main()
